0344241dc61c31b090c8ceeab024ac8475f3fbc0
WilliamTrung <williamthanhtrungq2@gmail.com>
2022-11-29 14:44:28 +0700

    Init Administrator Management project

--- AppService/Extension/Helper.cs
+++ AppService/Extension/Helper.cs
@@ -12,5 +12,18 @@ namespace AppService.Extension
         {
             return a.ToLower().Contains(b.ToLower());
         }
+        public static List<string> GetPictureId(string pictures)
+        {
+            //sample
+            //https://res.cloudinary.com/dvqruuvxs/image/upload/v1669643226/
+            List<string> result = new List<string>();
+            foreach(var picture in pictures.Split(","))
+            {
+                var split = picture.Split("/");
+                var id = split[7].Split(".")[0];
+                result.Add(id);
+            }
+            return result;
+        }
     }
 }
---
+++ KimLienAdministrator/Pages/ProductManagement/Index.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AppCore;
+using AppCore.Entities;
+
+namespace KimLienAdministrator.Pages.ProductManagement
+{
+    public class IndexModel : PageModel
+    {
+        private readonly AppCore.SqlContext _context;
+
+        public IndexModel(AppCore.SqlContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Product> Product { get;set; } = default!;
+
+        public async Task OnGetAsync()
+        {
+            if (_context.Products != null)
+            {
+                Product = await _context.Products.ToListAsync();
+            }
+        }
+    }
+}
