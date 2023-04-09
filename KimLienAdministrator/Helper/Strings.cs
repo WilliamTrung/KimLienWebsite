c8928d4d583320b5d27bf6ece634285125db1940
WilliamTrung <williamthanhtrungq2@gmail.com>
2023-04-09 17:04:35 +0700

    Update

--- KimLienAdministrator/Helper/Strings.cs
+++ KimLienAdministrator/Helper/Strings.cs
@@ -2,13 +2,17 @@
 {
     public static class Strings
     {
+        public static string MainBackground = "https://kimlien1808.blob.core.windows.net/pictures/backround.jpeg";
+        public static string MainBackgroundTemp = "https://kimlien1808.blob.core.windows.net/pictures/background_temp.jpeg";
         public static readonly string CategoryUpdateFailMessage = "This name is not valid!";
         public static readonly string CategoryDeleteFailMessage = "Failed to delete!";
         public static readonly string LoginFailed = "Sai Id hoặc mật khẩu!";
         public static readonly string NavProductIndex = "/ProductManagement/Index";
         public static readonly string NavCategoryIndex = "/CategoryManagement/Index";
+        public static readonly string NavUploadBGIndex = "/UploadPicture";
         public static readonly string NavProductIndexText = "Quản lý sản phẩm";
         public static readonly string NavCategoryIndexText = "Quản lý loại hàng";
+        public static readonly string NavUploadBGIndexText = "Tải ảnh nền mới";
         public static readonly string NavLogoutText = "Đăng xuất";
         public static readonly string NavLogout = "/Logout";
     }
