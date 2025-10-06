6280f19f9d94638808a2af7b8e8754f04143698c
Trung Arobid <trungnguyen.arobid@gmail.com>
2025-10-06 14:56:06 +0700

    modify file service abstractions

--- refactor/kim-lien-application/Common.Application.Storage/Abstraction/ICloudStorageService.cs
+++ refactor/kim-lien-application/Common.Application.Storage/Abstraction/ICloudStorageService.cs
@@ -4,8 +4,8 @@ namespace Common.Application.Storage.Abstraction
 {
     public interface ICloudStorageService
     {
-        Task UploadFile(FileDto file);
-        Task UploadFile(List<FileDto> files);
+        Task<string> UploadFile(FileDto file);
+        Task<List<string>> UploadFile(List<FileDto> files);
         Task DeleteFile(string fileUrl);
         Task MoveFile(string fileUrl, string destinationUrl);
     }
