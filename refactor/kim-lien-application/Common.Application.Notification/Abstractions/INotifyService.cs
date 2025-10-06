3cca17698c0d98041dcddd9a7c176b11e9c8dec8
Trung Arobid <trungnguyen.arobid@gmail.com>
2025-10-06 18:29:07 +0700

    add email service

---
+++ refactor/kim-lien-application/Common.Application.Notification/Abstractions/INotifyService.cs
@@ -0,0 +1,7 @@
+﻿namespace Common.Application.Notification.Abstractions
+{
+    public interface INotifyService
+    {
+        public Task PublishNotification(string title, string message, List<string> userIds, object? payload = null, CancellationToken ct = default);
+    }
+}
---
+++ refactor/kim-lien-application/Common.Infrastructure.EmailService/NotifyEmailService.cs
@@ -0,0 +1,12 @@
+﻿using Common.Application.Notification.Abstractions;
+
+namespace Common.Infrastructure.EmailService
+{
+    public class NotifyEmailService : INotifyService
+    {
+        public Task PublishNotification(string title, string message, List<string> userIds, object? payload = null, CancellationToken ct = default)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
