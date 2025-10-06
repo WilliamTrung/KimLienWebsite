cbf7801cc74e9e65ac19ae0770ee524cb529cdc5
Trung Arobid <trungnguyen.arobid@gmail.com>
2025-10-06 11:02:30 +0700

    add user management controller

---
+++ refactor/kim-lien-application/Admin.Api/Controllers/UserController.cs
@@ -0,0 +1,48 @@
+﻿using Admin.Application.Commands.User;
+using Common.Api;
+using Common.Kernel.Constants;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Admin.Api.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = Roles.Administrator)]
+    [Route("/api/management/user")]
+    public class UserController(ISender sender) : ControllerBase
+    {
+        [HttpPost]
+        [Route("create")]
+        public async Task<IActionResult> Create([FromBody] CreateUserCommand request, CancellationToken ct)
+        {
+            await sender.Send(request, ct);
+            return this.CreateOk();
+        }
+        [HttpPut]
+        [Route("modify")]
+        public async Task<IActionResult> Modify([FromBody] ModifyUserCommand request, CancellationToken ct)
+        {
+            await sender.Send(request, ct);
+            return this.CreateOk();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetPagination([FromQuery] QueryUserCommand request, CancellationToken ct)
+        {
+            var result = await sender.Send(request, ct);
+            return this.CreateOk(result);
+        }
+
+        [HttpGet]
+        [Route("{slug}")]
+        public async Task<IActionResult> GetDetail([FromRoute] string slug, CancellationToken ct)
+        {
+            var command = new GetDetailUserCommand
+            {
+                Value = slug,
+            };
+            var result = await sender.Send(command, ct);
+            return this.CreateOk(result);
+        }
+    }
+}
