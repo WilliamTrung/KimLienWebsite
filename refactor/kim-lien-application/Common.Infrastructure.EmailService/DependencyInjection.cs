7224d8be1314d1788a5f6b51127d637da5449d0d
LOLIIZABEZT <williamthanhtrungq2@gmail.com>
2025-10-08 20:30:41 +0700

    resolve command handler for email

--- refactor/kim-lien-application/Common.Infrastructure.EmailService/DependencyInjection.cs
+++ refactor/kim-lien-application/Common.Infrastructure.EmailService/DependencyInjection.cs
@@ -8,6 +8,7 @@ namespace Common.Infrastructure.EmailService
     {
         public static void RegisterEmailService(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
             services.Configure<SmtpOptions>(configuration.GetSection("SmtpOptions"));
             services.AddKeyedTransient<INotifyService, NotifyEmailService>(nameof(NotifyEmailService));
         }
---
+++ refactor/kim-lien-application/Common.Infrastructure.EmailService/Handlers/NotifyEmailCommandHandler.cs
@@ -0,0 +1,20 @@
+﻿using Common.Application.Notification.Abstractions;
+using Common.Infrastructure.EmailService.Commands;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Common.Infrastructure.EmailService.Handlers
+{
+    public class NotifyEmailCommandHandler : IRequestHandler<NotifyEmailCommand>
+    {
+        private readonly INotifyService _notifyService;
+        public NotifyEmailCommandHandler(IServiceProvider serviceProvider)
+        {
+            _notifyService = serviceProvider.GetRequiredKeyedService<INotifyService>(nameof(NotifyEmailService));
+        }
+        public async Task Handle(NotifyEmailCommand request, CancellationToken cancellationToken)
+        {
+            await _notifyService.PublishNotification(request.Title, request.Message, request.Emails, request.Payload, cancellationToken);
+        }
+    }
+}
