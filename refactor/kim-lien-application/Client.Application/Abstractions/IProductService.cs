c8f700b947d975fc48a47ae98a3dcbddedce1144
LOLIIZABEZT <williamthanhtrungq2@gmail.com>
2025-10-17 01:03:37 +0700

    implement product interface

--- refactor/kim-lien-application/Client.Application/Abstractions/IProductService.cs
+++ refactor/kim-lien-application/Client.Application/Abstractions/IProductService.cs
@@ -1,12 +1,13 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Client.Application.Models.Product;
+using Common.Kernel.Request.Pagination;
+using Common.Kernel.Response.Pagination;
 
 namespace Client.Application.Abstractions
 {
     public interface IProductService
     {
+        Task<PaginationResponse<ProductDto>> GetPaginationResponse(PaginationRequest<ProductFilterModel> request, CancellationToken ct);
+        Task<ProductDto> GetDetail(GetDetailProductRequest request, CancellationToken ct);
+        Task<List<ProductDto>> GetProductsByCategory(Guid categoryId, CancellationToken ct);
     }
 }
---
+++ refactor/kim-lien-application/Client.Application/Models/Product/ProductCategoryDto.cs
@@ -0,0 +1,8 @@
+﻿namespace Client.Application.Models.Product
+{
+    public class ProductCategoryDto
+    {
+        public Guid CategoryId { get; set; }
+        public string CategoryName { get; set; } = null!;
+    }
+}
