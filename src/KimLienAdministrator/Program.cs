e954f2f7f81a2db0084baaf0aa865002cbe303ac
LOLIIZABEZT <williamthanhtrungq2@gmail.com>
2025-11-25 00:48:57 +0700

    Revert "try add global IgnoreAntiforgeryTokenAttribute"
    This reverts commit b7542120780d5aaaa43824929aa3a247381a845d.

--- src/KimLienAdministrator/Program.cs
+++ src/KimLienAdministrator/Program.cs
@@ -7,16 +7,12 @@ using KimLienAdministrator;
 using KimLienAdministrator.Helper.Azure.Blob;
 using KimLienAdministrator.Helper.Azure.IBlob;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddRazorPages(x =>
-{
-    x.Conventions.ConfigureFilter(new IgnoreAntiforgeryTokenAttribute());
-});
+builder.Services.AddRazorPages();
 var configuration = new ConfigurationBuilder()
             .SetBasePath(Directory.GetCurrentDirectory()) // This is the line you would change if your configuration files were somewhere else
             .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
@@ -42,6 +38,7 @@ services.AddScoped<IProductBlob, ProductBlob>();
 services.AddTransient<PictureBlob>();
 services.AddSession(option => option.IdleTimeout = TimeSpan.FromMinutes(30));
 services.AddTransient<IAuthService, AuthService>();
+
 var blobStorage = configuration.GetSection("BlobStorage");
 var connection = blobStorage["AzureWebJobsStorage"];
 
